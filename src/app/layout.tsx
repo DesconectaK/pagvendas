f18a4c4a06d6182f6773ddc18cd4f8fca4dfa183
Leandro Melo <leandromdefreitas082@gmail.com>
2025-06-12 09:29:34 +0000

    Remova os pixels atuais que possam gerar conflito e atualize para esse n

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { Roboto } from 'next/font/google';
 const roboto = Roboto({
   subsets: ['latin'],
   weight: ['400', '700', '900'],
-  variable: '--font-roboto', // This will create a CSS variable
+  variable: '--font-roboto',
 });
 
 export const metadata: Metadata = {
@@ -24,22 +24,22 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        {/* VTurb Speed Optimization Links - VSL Section (Updated) */}
+        {/* VTurb Speed Optimization Links - VSL Section (Main VSL video) */}
         <link rel="preload" href="https://scripts.converteai.net/203430db-ad79-48e2-a8e6-4634be611b23/players/68473bf78ce134c08f091906/player.js" as="script" />
         <link rel="preload" href="https://scripts.converteai.net/lib/js/smartplayer/v1/smartplayer.min.js" as="script" />
         <link rel="preload" href="https://images.converteai.net/203430db-ad79-48e2-a8e6-4634be611b23/players/68473bf78ce134c08f091906/thumbnail.jpg" as="image" />
         <link rel="preload" href="https://cdn.converteai.net/203430db-ad79-48e2-a8e6-4634be611b23/68473bb3d17dfe0519c08193/main.m3u8" as="fetch" />
-
+        
         {/* Common DNS Prefetch */}
         <link rel="dns-prefetch" href="https://cdn.converteai.net" />
         <link rel="dns-prefetch" href="https://scripts.converteai.net" />
         <link rel="dns-prefetch" href="https://images.converteai.net" />
         <link rel="dns-prefetch" href="https://api.vturb.com.br" />
 
-        {/* Utmify Pixel Script */}
+        {/* Utmify Pixel Script - Updated */}
         <Script id="utmify-pixel-setup" strategy="afterInteractive">
           {`
-            window.pixelId = "684a98a46959ede40459dd61";
+            window.pixelId = "684a9db3e82a6cc4e52c43ae";
             var a = document.createElement("script");
             a.setAttribute("async", "");
             a.setAttribute("defer", "");
