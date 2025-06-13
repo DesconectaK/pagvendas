a3250a4cf642d80a9c4de7f64cfa59b268860bad
Leandro Melo <leandromdefreitas082@gmail.com>
2025-06-13 17:05:43 +0000

    Adicione ao Head: <link rel="preload" as="image" href="URL-DA-IMAGEM" ty

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import Script from 'next/script';
 import './globals.css';
 import ClientSideToaster from '@/components/ClientSideToaster';
-import { Roboto, Inter } from 'next/font/google'; // Corrected to use next/font
+import { Roboto } from 'next/font/google'; // Corrected to use next/font
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -30,10 +30,13 @@ export default function RootLayout({
         <link rel="preload" href="https://images.converteai.net/203430db-ad79-48e2-a8e6-4634be611b23/players/68473bf78ce134c08f091906/thumbnail.jpg" as="image" />
         <link rel="preload" href="https://cdn.converteai.net/203430db-ad79-48e2-a8e6-4634be611b23/68473bb3d17dfe0519c08193/main.m3u8" as="fetch" />
         
-        {/* VTurb Speed Optimization Links - Testimonial Video 1 */}
+        {/* VTurb Speed Optimization Links - Testimonial Video 1 (New) */}
         <link rel="preload" href="https://scripts.converteai.net/203430db-ad79-48e2-a8e6-4634be611b23/players/684b8fc8ad7cf9fd032dfd4a/player.js" as="script" />
         <link rel="preload" href="https://images.converteai.net/203430db-ad79-48e2-a8e6-4634be611b23/players/684b8fc8ad7cf9fd032dfd4a/thumbnail.jpg" as="image" />
         <link rel="preload" href="https://cdn.converteai.net/203430db-ad79-48e2-a8e6-4634be611b23/684b8fc3fb36cb9f760dcf4e/main.m3u8" as="fetch" />
+
+        {/* Image Preload (User added) */}
+        <link rel="preload" as="image" href="URL-DA-IMAGEM" type="image/webp" />
 
         {/* Common DNS Prefetch */}
         <link rel="dns-prefetch" href="https://cdn.converteai.net" />
